4780667a105fe4a146bc7410a01ed1343c23c603
정연우 <jung-yeon-woo@Yeonwoos-Mac-mini.local>
2023-12-22 02:22:35 +0900

    help needed seriously

--- internal/app/main.go
+++ internal/app/main.go
@@ -82,8 +82,8 @@ func main() {
 	if err != nil {
 		global.Logger.Error(fmt.Sprintf("Creating zmq4 subscriber failed: %v", err))
 	}
-	// err = global.Publisher.Connect(fmt.Sprintf("tcp://localhost:%v", global.Conf.PublishPort))
-	err = global.Publisher.Connect(fmt.Sprintf("tcp://127.0.0.1:%v", global.Conf.PublishPort))
+	// err = global.Publisher.Connect(fmt.Sprintf("tcp://127.0.0.1:%v", global.Conf.PublishPort))
+	err = global.Publisher.Bind(fmt.Sprintf("tcp://127.0.0.1:%v", global.Conf.PublishPort))
 	if err != nil {
 		global.Logger.Error(fmt.Sprintf("Connecting zmq4 publisher to tcp://127.0.0.1:%v failed: %v", global.Conf.PublishPort, err))
 	}
